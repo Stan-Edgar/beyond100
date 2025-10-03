431978887cbfc6e9594bc61da974245421629b2d
Stan <stanedgar@gmail.com>
2025-10-03 09:32:41 -0600

    finished cta section

--- my-landing/src/cta.jsx
+++ my-landing/src/cta.jsx
@@ -1,11 +1,12 @@
 import react from 'react';
+import icons from "./icons.js";
 
 export default function Cta() {
     return (
         
-        <div className='flex flex-col justify-center items-center'>
+        <div className='flex flex-col items-center mt-24 gap-6'>
             <h2 className='text-[3.375rem] tracking-[-0.20rem] flex flex-col text-center  leading-[1.1] font-bold'>Ready to grow <br />  your SaaS?</h2>
-        <div>Book a free discovery call <svg></svg></div>
+        <div className='flex px-5 py-2 gap-2.5 bg-[#813FF4] h-[2.6875rem] text-white font-semibold rounded-xl text-[1.125rem] cursor-pointer'>Book a free discovery call <img src={icons.discover} alt="top right arrow" width='14' height='14' /></div>
         </div>
         
     );
--- my-landing/src/footer.jsx
+++ my-landing/src/footer.jsx
@@ -1,17 +1,23 @@
 import react from 'react';
 import mail from "./assets/icons/mail.svg";
 import phone from "./assets/icons/Phone.svg";
+import Logo from './assets/Logo100.png';
 
 export default function Footer() {
     return (
-        <div>
+        <div className='flex w-full h-[169px] px-[5.5rem] py-10 mt-[5.5rem] font-manrope border'>
+            <svg width="1440" height="169" class="absolute top-0 left-0 w-[1440px] h-[169px]">
+    <rect x="0" y="0" width="1440" height="169" fill="none"
+      stroke="rgba(0,0,0,0.24)" stroke-width="1"
+      stroke-dasharray="134.75 134.75" />
+  </svg>
 
-            <div>
-                <img src="" alt="Beyond100 Logo" />
+            <div className='flex flex-col'>
+                <img src={Logo} alt="Beyond100 Logo" />
                 <p>© 2025 Beyond100.</p>
             </div>
 
-            <div>
+            <div className='flex flex-col'> 
                 <div>
                     <img src={mail} alt="email" />
                     <p>thestanedgar@gmail.com</p>
