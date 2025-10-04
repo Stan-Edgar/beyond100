03542caffa227f2473cfbbef9ffe1267e584ff92
Stan <stanedgar@gmail.com>
2025-10-03 21:21:55 -0600

    final jsx landing skeleton done for desktop

--- my-landing/src/faq.jsx
+++ my-landing/src/faq.jsx
@@ -1,65 +1,81 @@
 import React from 'react';
 import icons from "./icons.js";
 
+const q = 'flex w-[38.25rem] h-[4.25rem] pl-[2rem] pr-4 bg-white border-2 border-[#F0F2F9] rounded-xl text-[1.125rem]';
+const plus = 'w-11 h-11 bg-[#EAEAEC] rounded-full flex items-center justify-center';
+const wrap = 'flex justify-between items-center w-full';
+
 export default function Faq() {
-    return (
-        <>
-        <div className='flex flex-col items-center bg-black py-12 text-white gap-16 font-manrope'>
-            <h2 className='text-[3.375rem]  tracking-[-0.20rem] flex flex-col text-center  leading-[1.1] font-bold'>Let's answer <br /> your questions</h2>
+  return (
+    <div className="flex flex-col items-center bg-black py-12 text-white gap-16 font-manrope">
+      <h2 className="text-[3.375rem] tracking-[-0.20rem] flex flex-col text-center leading-[1.1] font-bold">
+        Let's answer <br /> your questions
+      </h2>
 
-            <div className='flex flex-col items-center text-black gap-4'>
-                <div className='flex w-[38.25rem] h-[4.25rem] items-center justify-between pl-[2rem] pr-4 bg-white border-2 border-[#F0F2F9] rounded-xl text-[1.125rem]'>
-                    <p>How long does it take?</p>
-                    <div className='w-11 h-11 bg-[#EAEAEC] rounded-full flex items-center justify-center '>
-                    <img src={icons.plus} width='16' height='16' />
-                    </div>
-                </div>
-
-                <div className='flex w-[38.25rem] h-[4.25rem] items-center justify-between pl-[2rem] pr-4 bg-white border-2 border-[#F0F2F9] rounded-xl text-[1.125rem]'>
-                 <p>How do I get started?</p>
-                 <div className='w-11 h-11 bg-[#EAEAEC] rounded-full flex items-center justify-center '>
-                    <img src={icons.plus} width='16' height='16' />
-                 </div>
-                </div>
-
-                <div className='flex w-[38.25rem] h-[4.25rem] items-center justify-between pl-[2rem] pr-4 bg-white border-2 border-[#F0F2F9] rounded-xl text-[1.125rem]'>
-                    <p>How does payment work?</p>
-                    <div className='w-11 h-11 bg-[#EAEAEC] rounded-full flex items-center justify-center '>
-                    <img src={icons.plus} width='16' height='16' />
-                 </div>
-                </div>
-
-                <div className='flex w-[38.25rem] h-[4.25rem] items-center justify-between pl-[2rem] pr-4 bg-white border-2 border-[#F0F2F9] rounded-xl text-[1.125rem]'>
-                    <p>Do you offer ongoing support?</p>
-                    <div className='w-11 h-11 bg-[#EAEAEC] rounded-full flex items-center justify-center '>
-                    <img src={icons.plus} width='16' height='16' />
-                 </div>
-                </div>
-
-                <div className='flex w-[38.25rem] h-[4.25rem] items-center justify-between pl-[2rem] pr-4 bg-white border-2 border-[#F0F2F9] rounded-xl text-[1.125rem]'>
-                    <p>Who do you work with?</p>
-                    <div className='w-11 h-11 bg-[#EAEAEC] rounded-full flex items-center justify-center '>
-                    <img src={icons.plus} width='16' height='16' />
-                 </div>
-                </div>
-
-                <div className='flex w-[38.25rem] h-[4.25rem] items-center justify-between pl-[2rem] pr-4 bg-white border-2 border-[#F0F2F9] rounded-xl text-[1.125rem]'>
-                    <p>What if I don't like it?</p>
-                    <div className='w-11 h-11 bg-[#EAEAEC] rounded-full flex items-center justify-center '>
-                    <img src={icons.plus} width='16' height='16' />
-                 </div>
-                </div>
-
-                <div className='flex w-[38.25rem] h-[4.25rem] items-center justify-between pl-[2rem] pr-4 bg-white border-2 border-[#F0F2F9] rounded-xl text-[1.125rem]'>
-                    <p>Do you offer refunds?</p>
-                    <div className='w-11 h-11 bg-[#EAEAEC] rounded-full flex items-center justify-center '>
-                    <img src={icons.plus} width='16' height='16' />
-                 </div>
-                </div>
+      <div className="flex flex-col items-center text-black gap-4">
+        <div className={q}>
+          <div className={wrap}>
+            <p>How long does it take?</p>
+            <div className={plus}>
+              <img src={icons.plus} width="16" height="16" />
             </div>
-            
+          </div>
         </div>
 
-        </>
-    );
-};
+        <div className={q}>
+          <div className={wrap}>
+            <p>How do I get started?</p>
+            <div className={plus}>
+              <img src={icons.plus} width="16" height="16" />
+            </div>
+          </div>
+        </div>
+
+        <div className={q}>
+          <div className={wrap}>
+            <p>How does payment work?</p>
+            <div className={plus}>
+              <img src={icons.plus} width="16" height="16" />
+            </div>
+          </div>
+        </div>
+
+        <div className={q}>
+          <div className={wrap}>
+            <p>Do you offer ongoing support?</p>
+            <div className={plus}>
+              <img src={icons.plus} width="16" height="16" />
+            </div>
+          </div>
+        </div>
+
+        <div className={q}>
+          <div className={wrap}>
+            <p>Who do you work with?</p>
+            <div className={plus}>
+              <img src={icons.plus} width="16" height="16" />
+            </div>
+          </div>
+        </div>
+
+        <div className={q}>
+          <div className={wrap}>
+            <p>What if I don't like it?</p>
+            <div className={plus}>
+              <img src={icons.plus} width="16" height="16" />
+            </div>
+          </div>
+        </div>
+
+        <div className={q}>
+          <div className={wrap}>
+            <p>Do you offer refunds?</p>
+            <div className={plus}>
+              <img src={icons.plus} width="16" height="16" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- my-landing/src/footer.jsx
+++ my-landing/src/footer.jsx
@@ -1,28 +1,22 @@
 import react from 'react';
 import mail from "./assets/icons/mail.svg";
 import phone from "./assets/icons/Phone.svg";
-import Logo from './assets/Logo100.png';
+import Logo from './assets/foologo.svg';
 
 export default function Footer() {
     return (
-        <div className='flex w-full h-[169px] px-[5.5rem] py-10 mt-[5.5rem] font-manrope border'>
-            <svg width="1440" height="169" class="absolute top-0 left-0 w-[1440px] h-[169px]">
-    <rect x="0" y="0" width="1440" height="169" fill="none"
-      stroke="rgba(0,0,0,0.24)" stroke-width="1"
-      stroke-dasharray="134.75 134.75" />
-  </svg>
-
-            <div className='flex flex-col'>
-                <img src={Logo} alt="Beyond100 Logo" />
+        <div className='flex w-full h-[169px] px-[5.5rem] py-10 mt-[5.5rem] justify-between font-manrope border-2 border-dashed border-black/25'>
+                <div className='flex flex-col gap-4'>
+                <img src={Logo} alt="Beyond100 Logo" width='100' height='100'/>
                 <p>© 2025 Beyond100.</p>
             </div>
 
-            <div className='flex flex-col'> 
-                <div>
+            <div className='flex flex-col gap-4'> 
+                <div className='flex gap-2'>
                     <img src={mail} alt="email" />
                     <p>thestanedgar@gmail.com</p>
                 </div>
-                <div>
+                <div className='flex gap-2'>
                     <img src={phone} alt="phone number" />
                     <p>501+ 605-2305</p>
                 </div>
